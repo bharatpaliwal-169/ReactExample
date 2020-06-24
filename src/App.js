80b62cb49f7e0b72aee1b8771a73a538f4818db1
Bharat <18uec169@lnmiit.ac.in>
2020-06-24 17:40:55 +0530

    learning react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+import Main from './components/MainComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {BrowserRouter} from 'react-router-dom';
+
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     dishes: DISHES
+  //   };
+  // }
+  render() {
+    return (
+        <BrowserRouter>
+          <div>
+            
+            <Main/>
+            
+          </div>
+        </BrowserRouter>
+      
+    );
+  }
 }
 
 export default App;
